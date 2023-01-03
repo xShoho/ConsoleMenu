3fcef3a01271ab90c5f803d01e80864fb42ada1a
Kamil Starosta <ks@g3w.eu>
2023-01-03 17:30:56 +0100

    Added title option and centered output

--- Menu.cs
+++ Menu.cs
@@ -1,12 +1,21 @@
 ﻿namespace Shoho.ConsoleMenu {
     public class Menu {
         private string[] elements = { };
+        private string title = "";
         private ConsoleColor foreground;
         private ConsoleColor background;
         private ConsoleColor highlightFg;
         private ConsoleColor highlightBg;
         private int longestElementSize = 0;
+        private int cursorX;
+        private int cursorY;
 
+
+        public string Title {
+            set {
+                this.title = value;
+            }
+        }
 
         public ConsoleColor Foreground {
             set {
@@ -45,6 +54,9 @@
             this.background = ConsoleColor.Blue;
             this.highlightFg = ConsoleColor.Blue;
             this.highlightBg = ConsoleColor.White;
+
+            this.cursorX = (Console.WindowWidth / 2) - ((this.longestElementSize / 2) + 5);
+            this.cursorY = (Console.WindowHeight / 2) - (this.elements.Length / 2);
         }
 
         public int Show() {
@@ -53,9 +65,32 @@
 
             int currentPosition = 0;
 
+            if(this.title != "") {
+                this.cursorY -= 1;
+                if(this.title.Length > this.longestElementSize) {
+                    this.longestElementSize = this.title.Length;
+                }
+            }
+
+            this.longestElementSize += 2;
+
+            Console.SetCursorPosition(this.cursorX, this.cursorY);
+
+            if(this.title != "") {
+                string s = this.title;
+                int paddingLeft = (this.longestElementSize - s.Length) / 2;
+                int paddingRight = this.longestElementSize - s.Length - paddingLeft;
+                
+
+                s = s.PadLeft(paddingLeft + s.Length).PadRight(this.longestElementSize);
+
+                Console.Write(s);
+                this.cursorY += 1;
+            }
+
             while(true) {
-                Console.SetCursorPosition(0, 0);
                 for(int i = 0; i < this.elements.Length; i++) {
+                    Console.SetCursorPosition(this.cursorX, this.cursorY + i);
                     if(i == currentPosition) {
                         Console.BackgroundColor = this.highlightBg;
                         Console.ForegroundColor = this.highlightFg;
@@ -65,9 +100,16 @@
                     }
 
                     string s = this.elements[i];
+                    int paddingLeft = (this.longestElementSize - s.Length) / 2;
+                    int paddingRight = this.longestElementSize - s.Length - paddingLeft;
 
-                    Console.WriteLine(s.PadLeft(5).PadRight(s.Length + 5 + this.longestElementSize + 3));
+
+                    s = s.PadLeft(paddingLeft + s.Length).PadRight(this.longestElementSize);
+
+                    Console.Write(s);
                 }
+
+                Console.SetCursorPosition(0, 0);
 
                 ConsoleKey pressed = Console.ReadKey().Key;
 
